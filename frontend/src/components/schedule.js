3713890a3441f93388b5b55cbb62b923aec33bab
isaque <isaque.roberto@deway.com.br>
2023-04-04 13:57:08 -0300

    att

--- frontend/src/components/schedule.js
+++ frontend/src/components/schedule.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Header, Button } from "semantic-ui-react";
 import moment from "moment";
 import { times } from "lodash";
 import './WeeklyAgenda.css';
+import { Typography } from '@mui/material';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
 
 const WeeklyAgenda = ({ events }) => {
   const [currentDate, setCurrentDate] = useState(moment());
@@ -32,62 +34,15 @@ const WeeklyAgenda = ({ events }) => {
   };
 
   return (
-    <div className="weekly-agenda">
-      <Header as="h1">
-        Agenda Semanal - {moment(currentDate).format("DD/MM/YYYY")}{" "}
-        <Button.Group floated="right">
-          <Button onClick={handlePrevWeek}>Anterior</Button>
-          <Button onClick={handleToday}>Hoje</Button>
-          <Button onClick={handleNextWeek}>Próxima</Button>
-        </Button.Group>
-      </Header>
-      <Grid className="agenda-table" celled>
-        <Grid.Row>
-          <Grid.Column width={3} className="agenda-cell"></Grid.Column>
-          {daysOfWeek.map((day, i) => (
-            <Grid.Column key={i} width={2} className="agenda-cell agenda-header">
-              {day}
-            </Grid.Column>
-          ))}
-        </Grid.Row>
-        {times(24, (i) => (
-          <Grid.Row key={i}>
-            <Grid.Column width={3} textAlign="center" className="agenda-cell agenda-header">
-              {`${i}:00`}
-            </Grid.Column>
-            {daysOfWeek.map((day, j) => (
-              <Grid.Column key={`${i}-${j}`} width={2} className="agenda-cell">
-                {events &&
-                  events.map((event) => {
-                    const start = moment(event.start);
-                    const end = moment(event.end);
-                    const dayOfWeek = moment(day, "DD/MM/YYYY");
-                    if (
-                      start.isSameOrAfter(dayOfWeek, "day") &&
-                      end.isSameOrBefore(dayOfWeek, "day") &&
-                      start.hours() <= i &&
-                      end.hours() >= i + 2
-                    ) {
-                      return (
-                        <div key={event.title} className="agenda-event">
-                          <Header as="h5" className="agenda-event-title">
-                            {event.title}
-                          </Header>
-                          <p className="agenda-event-time">{`${start.format(
-                            "HH:mm"
-                          )} - ${end.format("HH:mm")}`}</p>
-                        </div>
-                      );
-                    }
-                    return null;
-                  })}
-              </Grid.Column>
-            ))}
-          </Grid.Row>
-        ))}
-      </Grid>
-    </div>
-  );
-};
+    <Card style={{ width: "40%", height: "15%", fontFamily: "sans-serif", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <Button variant="solid" >
+        <Typography align="center" >
+          agendar
+        </Typography>
+      </Button>
+    </Card>
+  
+        )
+  }
 
 export default WeeklyAgenda;
--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import { React, useState } from 'react';
 import WeeklyAgenda from './schedule';
+import ButtonAppBar from './appbar';
 
 function UserWelcome({ user }) {
 
   return (
-    <div style={{ backgroundColor: '#fffff', color: '#000000', padding: '20px', fontFamily: 'sans-serif' }}>
-      <h2>Olá, {user.nome}!</h2>
-      <p>Bem-vindo(a) à barbearia {user.nome_barbearia}.</p>
-      <WeeklyAgenda/>        
+    <div>
+      <ButtonAppBar user={user}/>
+      <div style={{ backgroundColor: '#fffff', color: '#000000', padding: '20px', fontFamily: 'sans-serif' }}>
+        <h2>Olá, {user.nome}!</h2>
+        <p>Bem-vindo(a) à barbearia {user.nome_barbearia}.</p>
+        <WeeklyAgenda/>     
+      </div>
     </div>
   );
 }
